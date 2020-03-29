ae7e2be731fb564246e451542d9a4dd335d6ec3e
Andrey Chekmarev <assminog@gmail.com>
2020-03-29 21:25:01 +0300

    Contacts array filled by user.

---
+++ HFIC_CreatorsRespect/arrElectronicContactsForPublish.php
@@ -0,0 +1,13 @@
+<?php
+$arrElectronicContactsForPublish= array(
+	'Email'		=> 'assminog@gmail.com',
+	//	'actionTemplate'	=> 'ElectronicContactServicesTemplates',
+	'Telegram'	=> 'hifiint',
+	'Whatsapp'	=> '+79117874457',
+	'Viber'		=> '',
+	'Skype'		=> '',
+	'ICQ'		=> '',
+	'VK'		=> 'assminog',
+	'Facebook'	=> 'assminog',
+	);
+?>
