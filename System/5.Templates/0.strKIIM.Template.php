de04bd4650e0b6d24630790737f9c42782d32eef
Andrey Chekmarev <assminog@gmail.com>
2020-07-15 15:42:19 +0300

    Initial.

---
+++ System/5.Templates/0.strKIIM.Template.php
@@ -0,0 +1,21 @@
+<?php
+/*
+<!--/\ © A.A.CheckMaRev tubmulur@yandex.ru
+  <  **>
+    Jl-->
+*/
+$strKIIMWindowHTML='
+	<DebugInfo
+		id="KIIMDebug"
+		class="blink"
+		style="
+			height:179px; 
+			width:98vw;
+			background-color:#fffc;
+			display:block;
+			overflow: scroll;
+			"
+		>
+	</DebugInfo>
+	';
+?>
