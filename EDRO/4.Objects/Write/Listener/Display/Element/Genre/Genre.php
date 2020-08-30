d081389a32b307ee1fdb7c87b54f3fe9d7876513
Andrey Chekmarev <assminog@gmail.com>
2020-08-31 01:27:46 +0300

    Update 1 of 2

--- EDRO/4.Objects/Write/Listener/Display/Element/Genre/Genre.php
+++ EDRO/4.Objects/Write/Listener/Display/Element/Genre/Genre.php
@@ -57,95 +57,19 @@ class Genre
 		{
 		$strO.=
 		'<'.strtolower(__CLASS__).
-			Overlay::strHTML().
 			$this->strEvent().
 			$this->strDesign().
 			'>';
+			Overlay::strHTML().
 			$strO.=
 			$this->strReality();
 		$strO.=
 		'</'.strtolower(__CLASS__).'>';
+		return $strO
 /*-[.]*/	}
-	/*private function strDesign()
-		{
-		}*/
-		/*if(сДляСравнения($arrParams['strStyle'])==сДляСравнения($strStyle))
-			{
-			$this->strHTML.='
-			<a
-				href="/search'.strLinkGroove($arrParams, 'strStyle').'"
-				class="sensor block rel left border-right border-bottom"
-				style="
-					color		:#4d4d4d;
-					background-color:#e1dfdf;
-					padding-left	:5px;
-					padding-right	:5px;
-					text-align	:center;
-					height		:20px;
-					text-decoration	:none;
-					"
-				>'.
-				$strStyle.
-				'<overlay 
-					class="abs block layer_5_Overlay" 
-					style="
-						top		:0;
-						left		:0;
-						text-align	:center;
-						width		:100%;
-						height		:100%;
-						color		:#fff;
-						background-color:rgba(240, 240, 240, 0.6);
-						"
-					onclick="
-						//objSearch.objValueInputStyle.value=\'\';
-						objEvent.arrParams.strStyle=\'\';
-						objEvent._UpdateURLDyn();
-						return false;
-						"
-					>
-					x
-				</overlay>
-					'.
-			'</a>';
-			}
-		else
-			{
-			if(!empty($strStyles)&&strpos('!!',$strStyles)!==FALSE)
-				{
-				$strColor='red';
-				}
-			else
-				{
-				$strColor='#4d4d4d';
-				}
-			$this->strHTML.='
-			<a
-				href="/search'.strLinkGroove($arrParams, 'strStyle', $strStyles, true).'"
-				class="sensor block left border-right border-bottom"
-				style="
-				color		:'.$strColor.';
-				background-color:#e1dfdf;
-			
-				padding-left		:5px;
-				padding-right		:5px;
-				text-align	:center;
-				height		:20px;
-				text-decoration	:none;
-				"
-				onclick="
-				//objSearch.objValueInputStyle.value=\''.$strStyles.'\';
-							objEvent.arrParams.strStyle=\''.$strStyles.'\';
-							objEvent._UpdateURLDyn();
-							return false;
-							"
-				>'.
-				$strStyle.
-			'</a>';
-			}
-		
-		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
-		}*/
+		//if(сДляСравнения($arrParams['strStyle'])==сДляСравнения($strStyle))
+		//href="/search'.strLinkGroove($arrParams, 'strStyle').'"
+		//class="sensor block rel left border-right border-bottom"
 	public static function strHTML($_objKIIM, $_strStyle, $arrParams)
 		{
 		$obj=new Genre($_objKIIM, $_strStyle, $arrParams);
