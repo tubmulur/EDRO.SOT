d42e026cb7f2395d8844900a1d9a315ad2bbd023
Andrey Chekmarev <assminog@gmail.com>
2020-09-16 02:44:52 +0300

    Add files via upload

--- System/5.Templates/0.strKIIM.Template.php
+++ System/5.Templates/0.strKIIM.Template.php
@@ -5,17 +5,30 @@
     Jl-->
 */
 $strKIIMWindowHTML='
-	<DebugInfo
-		id="KIIMDebug"
-		class="blink"
+	<debugWindow
+		class="V1 fixed block layer_4_1 scrollerY no-select"
 		style="
-			height:179px; 
-			width:98vw;
-			background-color:#fffc;
-			display:block;
-			overflow: scroll;
+			font-size		:xx-small;
+			width			:100%;
+			height			:20px;
+			background-color	:white;
 			"
 		>
-	</DebugInfo>
+		<heading 
+			class="block left no-select" 
+			style="
+				color:#fff;
+				margin-right:2px;
+				background-color:red;
+				"
+			>
+			vKIIM: 
+		</heading>
+		<strdata 
+			id="strPlayerPlayEventsDebugString"
+			class="block  left  no-select"
+			>
+		</strdata>
+	</debugWindow>
 	';
 ?>
